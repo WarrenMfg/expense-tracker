1f2678f4949e1b1118010b8d01d8e36d0aaf753b
Robin Kim <robincodes@pm.me>
2019-11-21 11:29:48 -0600

    Create helper methods for localStorage
    :)

--- app/app.js
+++ app/app.js
@@ -11,27 +11,39 @@
 
 // Local Storage Utility Functions
 //get item
-  // code goes here
+var getItem = function(key) {
+  return window.localStorage.getItem(key);
+};
 
 //create
-  // code goes here
+var createItem = function(key, value) {
+  window.localStorage.setItem(key, value);
+};
 
 //update
-  // code goes here
+var updateItem = function(key, value) {
+  window.localStorage.setItem(key, value);
+};
 
 //delete
-  // code goes here
+var deleteItem = function(key) {
+  window.localStorage.removeItem(key);
+};
 
 //clear everything
-  // code goes here
+var clearEverything = function() {
+  window.localStorage.clear();
+};
 
-// key exists? 
-  // code goes here
+// key exists?
+var keyExists = function(key) {
+  var currentValue = getItem(key);
+  return currentValue !== null;
+};
 
 
 ///////////////////////////////////////////
 $(document).ready(function() {
-
  // click event listener for create
     // code goes here
 
